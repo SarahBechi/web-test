f2f87819bbd21036852057d4afd1a8b688c97906
SarahBechi <59558883+SarahBechi@users.noreply.github.com>
2020-04-20 18:08:46 +0000

    Updates

---
+++ precache-manifest.49853ca0b9063fee3594f7adb64a2c8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfaa4d31d7fa0567b44dd5056c3e4ba1",
+    "url": "/web-test/index.html"
+  },
+  {
+    "revision": "07763a2c76069737295c",
+    "url": "/web-test/static/css/main.bc43850f.chunk.css"
+  },
+  {
+    "revision": "b8a82ef6ef7795e4c8b8",
+    "url": "/web-test/static/js/2.e5f605b0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/web-test/static/js/2.e5f605b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07763a2c76069737295c",
+    "url": "/web-test/static/js/main.5f536419.chunk.js"
+  },
+  {
+    "revision": "de44a570b48243bdb888",
+    "url": "/web-test/static/js/runtime-main.82ea08b6.js"
+  }
+]);
